ff791cf12d995fde6f9ea7b952c804393f941f1f
Aaron Schumacher <ajschumacher@gmail.com>
2014-07-29 18:47:31 +0000

    write function def and docstring

---
+++ selfcontain.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def selfcontain(html_string):
+    """Make HTML self-contained
+
+    Take an HTML string and return an HTML string with external
+    dependencies as much as possible removed.
+
+    Parameter:
+    html_string : str, required
+        HTML text, possibly with links to JavaScript, CSS, and images
+        that would require HTTP calls for rendering
+    """
+    html_string = ""
+    return html_string
