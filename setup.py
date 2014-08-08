d116e8372584df445ad79ae7385fa2cf3e2ba649
Aaron Schumacher <ajschumacher@gmail.com>
2014-08-08 14:16:55 +0000

    initial setup things

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name = 'selfcontain',
+      packages = ['selfcontain'],
+      description = 'shoe-horn a web page into a single html file',
+      license = 'MIT',
+      author = 'Aaron Schumacher',
+      author_email = 'ajschumacher@gmail.com',
+      url = 'https://github.com/ajschumacher/selfcontain',
+      download_url = 'https://github.com/ajschumacher/selfcontain/tarball/0.1',
+      version = '0.1',
+      install_requires = ['lxml', 'requests', 'slimit', 'rcssmin'])
