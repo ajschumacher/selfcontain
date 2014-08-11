448fcb7e2d84dd729a36c6f6b88b843adee7a594
Aaron Schumacher <ajschumacher@gmail.com>
2014-08-11 15:17:28 +0000

    update to read README.rst for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(name = 'selfcontain',
       packages = ['selfcontain'],
       description = 'shoe-horn a web page into a single html file',
+      long_description = long_description,
       license = 'MIT',
       author = 'Aaron Schumacher',
       author_email = 'ajschumacher@gmail.com',
